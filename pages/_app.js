6fed6fd4a8de85980418eb0e1083d570ce80b307
lazargas <82470472+lazargas@users.noreply.github.com>
2022-12-18 01:06:28 +0530

    tenth commit

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import '../styles/globals.css';
+
+//internal import
+import {VotingProvider} from '../context/Voter';
+import NavBar from '../components/NavBar/NavBar';
+
+const MyApp = ({ Component, pageProps }) => (
+  
+  <VotingProvider>
+    <div>
+      <NavBar />
+      <div>
+        <Component {...pageProps} />;
+      </div>
+      </div>
+    
+  </VotingProvider>
+  
+  
+);
+  
+
+export default MyApp;
